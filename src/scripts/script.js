1b6a7c80ebeecc6c9a34670aa563b182f32f1b73
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-04-11 21:27:21 -0300

    update styles and script

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,28 +1,45 @@
 // Seleção de elementos
 const todoForm = document.querySelector("#todo-form");
-const todoInput = document.querySelector("todo-input");
-const todoList = document.querySelector("todo-list");
-const editForm = document.querySelector("edit-form");
-const editInput = document.querySelector("edit-input");
-const cancelEditBtn = document.querySelector("cancel-edit-btn");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // Funções
 const saveTodo = (text) => {
-	const todo = document.createElement("div")
-	todo.classList.add("todo")
+	const todo = document.createElement("div");
+	todo.classList.add("todo");
 
-	const todoTitle = document.createElement("h3")
-	todoTitle.innerHTML = text
+	const todoTitle = document.createElement("h3");
+	todoTitle.innerHTML = text;
+	todo.appendChild(todoTitle);
+
+	const doneBtn = document.createElement("button");
+	doneBtn.classList.add("finish-todo");
+	doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+	todo.appendChild(doneBtn);
+
+	const editBtn = document.createElement("button");
+	editBtn.classList.add("edit-todo");
+	editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+	todo.appendChild(editBtn);
+	
+	const deleteBtn = document.createElement("button");
+	deleteBtn.classList.add("delete-todo");
+	deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+	todo.appendChild(deleteBtn);
+
+	todoList.appendChild(todo);
 }
 
 // Eventos
-todoForm.addEventListener("submit",(e) => {
+todoForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	const inputValue = todoInput.value;
 
 	if (inputValue) {
-		saveTodo(inputValue);
+		saveTodo(inputValue)
 	}
-
 })
